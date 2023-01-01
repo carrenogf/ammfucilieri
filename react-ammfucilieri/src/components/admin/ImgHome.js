0339d66da0bf3e2cd89a8f76b291f74d999d6a45
carrenogf <carrenogf@gmail.com>
2023-01-01 12:41:20 -0300

    Añadido get url imagen firebase

--- react-ammfucilieri/src/components/admin/ImgHome.js
+++ react-ammfucilieri/src/components/admin/ImgHome.js
@@ -1,18 +1,25 @@
-import {uploadImgHome} from '../../firebase/config'
-import { imgUrls } from './fields';
+import {uploadImgHome,getImgUrl} from '../../firebase/config'
+// import { imgUrls } from './fields';
 import { useState, useEffect} from 'react'
+
 export const ImgHome = ()=>{
   const [img1,setImg1] = useState(null);
   const [img2,setImg2] = useState(null);
   const [img3,setImg3] = useState(null);
+  const [img1Url,setImg1Url] = useState(null);
+  const [img2Url,setImg2Url] = useState(null);
+  const [img3Url,setImg3Url] = useState(null);
 
   useEffect(()=>{
+    getImgUrl('img/home/imgHome1').then(url => setImg1Url(url))
+    getImgUrl('img/home/imgHome2').then(url => setImg2Url(url))
+    getImgUrl('img/home/imgHome3').then(url => setImg3Url(url))
+
     if(img1){uploadImgHome(img1,'imgHome1').then(()=>window.location.reload())}
     if(img2){uploadImgHome(img2,'imgHome2').then(()=>window.location.reload())}
     if(img3){uploadImgHome(img3,'imgHome3').then(()=>window.location.reload())}
-    
-  },[img1,img2,img3])
-
+  },[img1Url,img2Url,img3Url,img1,img2,img3])
+  
   return (
     <div id="accordion">
       <div className="card">
@@ -29,7 +36,7 @@ export const ImgHome = ()=>{
             <div className="row">
               <div className="col-12 col-sm-4">
                 <div className='fcc' style={{maxHeight:"200px",overflow:"hidden"}}>
-                  <img className='img-fluid' src={imgUrls.home1.url} alt=""/>
+                  <img className='img-fluid' src={img1Url} alt=""/>
                 </div>
                 <p>Imagen 1</p>
                 <input type="file" className="form-control" onChange={e=>setImg1(e.target.files[0])}/>
@@ -37,7 +44,7 @@ export const ImgHome = ()=>{
 
               <div className="col-12 col-sm-4">
                 <div className='fcc' style={{maxHeight:"200px",overflow:"hidden"}}>
-                  <img className='img-fluid' src={imgUrls.home2.url} alt=""/>
+                  <img className='img-fluid' src={img2Url} alt=""/>
                 </div>
                 <p>Imagen 2</p>
                 <input type="file" className="form-control" onChange={e=>setImg2(e.target.files[0])}/>
@@ -45,7 +52,7 @@ export const ImgHome = ()=>{
 
               <div className="col-12 col-sm-4">
                 <div className='fcc' style={{maxHeight:"200px",overflow:"hidden"}}>
-                  <img className='img-fluid' src={imgUrls.home3.url} alt=""/>
+                  <img className='img-fluid' src={img3Url} alt=""/>
                 </div>
                 <p>Imagen Fondo Formulario Contacto</p>
                 <input type="file" className="form-control" onChange={e=>setImg3(e.target.files[0])}/>
--- react-ammfucilieri/src/firebase/config.js
+++ react-ammfucilieri/src/firebase/config.js
@@ -1,14 +1,19 @@
 import { initializeApp } from "firebase/app";
-import { getStorage,ref, uploadBytes} from 'firebase/storage';
+import { getStorage,ref, uploadBytes,getDownloadURL} from 'firebase/storage';
+import * as firebase from "firebase/app";
 
 
-
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const storage = getStorage(app)
 
 export async function uploadImgHome(file,name){
   const storageRef = ref(storage,'img/home/'+name);
   await uploadBytes(storageRef,file);
+}
+
+
+export  function getImgUrl(file) {
+  const urlimg =  getDownloadURL(ref(storage, file))
+  return urlimg
 
 }
